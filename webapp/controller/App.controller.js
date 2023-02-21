b1109aacb9503e723b29dd142fc0d074435c1f18
cfvaz1973 <cfvaz1973@gmail.com>
2023-02-21 19:19:09 +0000

    21-02-2023 (1)

--- webapp/controller/App.controller.js
+++ webapp/controller/App.controller.js
@@ -3,10 +3,13 @@ sap.ui.define(
     "sap/ui/core/mvc/Controller",
     "sap/m/MessageToast",
     "sap/ui/model/json/JSONModel",
+    "sap/ui/model/resource/ResourceModel",
   ],
-  function (Controller, MessageToast, JSONModel) {
+  function (Controller, MessageToast, JSONModel, ResourceModel) {
     "use strict";
     return Controller.extend("sap.ui.demo.walkthrough.controller.App", {
+      /*
+// onInit function removed from the controller. Now it's done at component level      
       onInit: function () {
         // set data model on the view
         let oData = {
@@ -16,9 +19,23 @@ sap.ui.define(
         };
         let oModel = new JSONModel(oData);
         this.getView().setModel(oModel);
+        // set i18n model on view
+        let i18nModel = new ResourceModel({
+          bundleName: "sap.ui.demo.walkthrough.i18n.i18n",
+        });
+        this.getView().setModel(i18nModel, "i18n");
       },
+*/
       onShowHello: function () {
-        MessageToast.show("Hello World");
+        // read msg from i18n model
+        let oBundle = this.getView().getModel("i18n").getResourceBundle();
+        let sRecipient = this.getView()
+          .getModel()
+          .getProperty("/recipient/name");
+        let sMsg = oBundle.getText("helloMsg", [sRecipient]);
+        // show message
+        MessageToast.show(sMsg);
+        //MessageToast.show("Hello World");
       },
     });
   }
--- webapp/index.js
+++ webapp/index.js
@@ -1,3 +1,6 @@
+/*
+// Now instead of the view, we create a component container,
+// which instatiates the view
 sap.ui.define(["sap/ui/core/mvc/XMLView"], function (XMLView) {
   "use strict";
 
@@ -7,3 +10,19 @@ sap.ui.define(["sap/ui/core/mvc/XMLView"], function (XMLView) {
     oView.placeAt("content");
   });
 });
+*/
+
+sap.ui.define(
+  ["sap/ui/core/ComponentContainer"],
+  function (ComponentContainer) {
+    "user strict";
+
+    new ComponentContainer({
+      name: "sap.ui.demo.walkthrough",
+      settings: {
+        id: "walkthrough",
+      },
+      async: true,
+    }).placeAt("content");
+  }
+);
